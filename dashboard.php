b746216b60b10f9e3f54c2f5b84125f3cc45146a
Ruba Alkhateeb <52524284+RubaAlkhateeb@users.noreply.github.com>
2020-08-20 16:04:37 +0300

    finished view all

--- dashboard.php
+++ dashboard.php
@@ -253,7 +253,7 @@
 													<span class="card-label font-weight-bolder text-dark">Latest Transaction</span>
 													<!-- <span class="text-muted mt-3 font-weight-bold font-size-sm">More than 400+ new members</span> -->
 												</h3>
-												<div class="card-toolbar">
+												<!-- <div class="card-toolbar">
 													<ul class="nav nav-pills nav-pills-sm nav-dark-75">
 														<li class="nav-item">
 															<a class="nav-link py-2 px-4 active" data-toggle="tab" href="#kt_tab_pane_3_1">All</a>
@@ -265,7 +265,7 @@
 															<a class="nav-link py-2 px-4 " data-toggle="tab" href="#kt_tab_pane_3_3">Expenses</a>
 														</li>
 													</ul>
-												</div>
+												</div> -->
 											</div>
 											<!--end::Header-->
 											<!--begin::Body-->
@@ -289,7 +289,7 @@
 														die("Connection failed: " . mysqli_connect_error());
 													}
 													
-													$sql = "SELECT * FROM Expenses ORDER BY id DESC LIMIT 0,5";
+													$sql = "SELECT * FROM Expenses ORDER BY id DESC LIMIT 0,10";
 													$result = $conn->query($sql);
 													
 													if ($result->num_rows > 0) {
@@ -315,8 +315,8 @@
 																<th class="p-0" style="min-width: 150px"></th>
 																<th class="p-0" style="min-width: 120px"></th>
 																<th class="p-0" style="min-width: 70px"></th>
-																<th class="p-0" style="min-width: 70px"></th>
-																<th class="p-0" style="min-width: 50px"></th>
+																<!-- <th class="p-0" style="min-width: 70px"></th>
+																<th class="p-0" style="min-width: 50px"></th> -->
 															</tr>
 														</thead>
 														<tbody>
@@ -341,21 +341,21 @@
 																<!-- <td class="text-right">
 																	<span class="font-weight-bolder text-primary"></span>
 																</td> -->
-																<td class="text-right pr-0" >
+																<!-- <td class="text-right pr-0" >
 																	<a href="#" class="btn btn-icon btn-light btn-sm" >
-																		<span class="svg-icon svg-icon-md svg-icon-success">
+																		<span class="svg-icon svg-icon-md svg-icon-success">-->
 																			<!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
-																			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+																			<!--<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 																				<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 																					<polygon points="0 0 24 0 24 24 0 24" />
 																					<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
 																					<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
 																				</g>
-																			</svg>
+																			</svg>-->
 																			<!--end::Svg Icon-->
-																		</span>
+																		<!--</span>
 																	</a>
-																</td>
+																</td> -->
 															</tr>
 														</tbody>
 													</table>
